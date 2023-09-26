e73ff2e83fa4e4eaa0e523404ccacfa45c6dd458
Adam Drew <addrew@redhat.com>
2023-09-26 15:26:54 -0400

    Multiple get data improvements

--- get_data.py
+++ get_data.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import time
 
 def read_token_from_file(file_path="token.txt"):
     with open(file_path, 'r') as file:
@@ -15,38 +14,55 @@ HEADERS = {
     "Authorization": f"token {TOKEN}"
 }
 
-def safe_request(url, headers, retries=3, backoff_factor=0.3):
-    for i in range(retries):
-        try:
-            response = requests.get(url, headers=headers, timeout=10)
-            response.raise_for_status()  # raises an HTTPError if one occurred
-            return response
-        except requests.exceptions.HTTPError as http_err:
-            # Handling rate limits
-            if response.status_code == 429:
-                print("Rate limit exceeded. Waiting...")
-                time.sleep(60)  # Wait for 60 seconds before retrying
-            else:
-                print(f"HTTP error occurred: {http_err}")
-        except Exception as err:
-            print(f"Error occurred: {err}")
-        time.sleep(backoff_factor)
-    return None
+def has_readme(repo, headers):
+    """Checks if a repository has a README.md file."""
+    readme_url = repo["contents_url"].replace("{+path}", "README.md")
+    readme_response = requests.head(readme_url, headers=headers)  # Using HEAD request to check existence
+    return readme_response.status_code == 200
+
 
 def get_all_repos(base_url, headers):
     repos = []
     page_num = 1
     while base_url:
         print(f"Fetching repositories from page {page_num}...")
-        response = safe_request(base_url, headers)
-        if response:
-            repos.extend(response.json())
-            base_url = response.links.get("next", {}).get("url")  # Pagination
-            page_num += 1
-        else:
-            base_url = None  # End loop if max retries reached
+        response = requests.get(base_url, headers=headers)
+        if response.status_code != 200:
+            raise ValueError(f"Failed to fetch repos. Status code: {response.status_code}")
+        repos.extend([r for r in response.json() if not r['name'].endswith('-build')])  # Skip repos ending in '-build'
+        base_url = response.links.get("next", {}).get("url")  # Pagination
+        page_num += 1
     print(f"Total repositories fetched: {len(repos)}")
     return repos
+
+def get_commit_history(commits_url, headers, max_commits=100):
+    commit_data = []
+    commit_count = 0
+
+    while commits_url and commit_count < max_commits:
+        commits_response = requests.get(commits_url, headers=headers)
+        
+        if commits_response.status_code == 200:
+            for commit in commits_response.json():
+                if commit_count >= max_commits:
+                    break
+
+                commit_info = {
+                    "contributor": commit["commit"]["committer"]["name"],
+                    "date": commit["commit"]["committer"]["date"],
+                    "message": commit["commit"]["message"]
+                }
+
+                commit_data.append(commit_info)
+                commit_count += 1
+
+            commits_url = commits_response.links.get("next", {}).get("url")  # Pagination for commits
+        else:
+            print(f"Failed to fetch commits. Status code: {commits_response.status_code}")
+            break
+
+    return commit_data
+
 
 def get_readme_and_history(repo, headers):
     repo_name = repo['name']
@@ -55,27 +71,18 @@ def get_readme_and_history(repo, headers):
     # Fetching README
     print(f"Fetching README for {repo_name}...")
     readme_url = repo["contents_url"].replace("{+path}", "README.md")
-    readme_response = safe_request(readme_url, headers)
+    readme_response = requests.get(readme_url, headers=headers)
     
-    if readme_response:
-        readme_content = readme_response.json().get("content", None)
+    if readme_response.status_code == 200:
+        readme_content = readme_response.json()["content"]
     else:
+        print(f"Failed to fetch README for {repo_name}. Status code: {readme_response.status_code}")
         readme_content = None
     
     # Fetching Git History
     print(f"Fetching commit history for {repo_name}...")
     commits_url = repo["commits_url"].replace("{/sha}", "")
-    commits_response = safe_request(commits_url, headers)
-    
-    commit_data = []
-    if commits_response:
-        for commit in commits_response.json()[:10]:  # limit to the most recent 10 commits
-            commit_info = {
-                "contributor": commit["commit"]["committer"]["name"],
-                "date": commit["commit"]["committer"]["date"],
-                "message": commit["commit"]["message"]
-            }
-            commit_data.append(commit_info)
+    commit_data = get_commit_history(commits_url, headers)
     
     return {
         "repo_name": repo_name,
@@ -88,8 +95,15 @@ repos = get_all_repos(BASE_URL, HEADERS)
 data = []
 for idx, repo in enumerate(repos, 1):
     print(f"\n[{idx}/{len(repos)}] Processing repo: {repo['name']}")
+    
+    # Check for README existence
+    if not has_readme(repo, HEADERS):
+        print(f"No README found for {repo['name']}. Skipping...")
+        continue
+
     repo_data = get_readme_and_history(repo, HEADERS)
     data.append(repo_data)
+
 
 print("\nSaving data to repos_data.json...")
 with open("data/repos_data.json", "w") as f:
