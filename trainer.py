eb9b229f3f5a0841ad59ad146521c186b3beda0b
Adam Drew <addrew@redhat.com>
2023-09-27 16:27:09 -0400

    Stuff

--- trainer.py
+++ trainer.py
@@ -1,16 +1,51 @@
+import json
+import base64
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, TrainingArguments, Trainer
+
+# Function to decode base64 encoded data
+def decode_base64(data):
+    return base64.b64decode(data).decode('utf-8')
+
+# Preprocess data from JSON file
+def preprocess_data(file_path):
+    with open(file_path, 'r', encoding="utf-8") as file:
+        repos_data = json.load(file)
+
+    repo_info_list = []
+
+    for repo in repos_data:
+        repo_info = "Repo Name: " + repo['repo_name'] + "\n"
+        
+        if 'readme_content' in repo and repo['readme_content']:
+            repo_info += "README Content:\n" + decode_base64(repo['readme_content']) + "\n"
+        
+        if 'languages' in repo:
+            repo_info += "Languages: " + ', '.join([k for k in repo['languages']]) + "\n"
+        
+        if 'top_contributors' in repo:
+            repo_info += "Top Contributors: " + ', '.join(repo['top_contributors']) + "\n"
+
+        repo_info_list.append(repo_info)
+
+    return "\n".join(repo_info_list)
+
+# Preprocess and save the data
+data = preprocess_data('data/repos_data.json')
+with open("temp_training_data.txt", "w", encoding="utf-8") as temp_file:
+    temp_file.write(data)
 
 # Load pretrained model and tokenizer
 MODEL_NAME = "gpt2-medium"
 tokenizer = GPT2Tokenizer.from_pretrained(MODEL_NAME)
 model = GPT2LMHeadModel.from_pretrained(MODEL_NAME)
 
-# Preprocess dataset
+# Create training dataset
 train_dataset = TextDataset(
     tokenizer=tokenizer,
-    file_path="data/knowledge_base.txt",
-    block_size=256  # Increased block size for more context
+    file_path="temp_training_data.txt",
+    block_size=256
 )
+
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False
@@ -18,14 +53,14 @@ data_collator = DataCollatorForLanguageModeling(
 
 # Define training arguments
 training_args = TrainingArguments(
-    per_device_train_batch_size=2,  # Adjust as needed
-    num_train_epochs=3,  # Increase epochs for more iterations
+    per_device_train_batch_size=2,
+    num_train_epochs=3,
     logging_dir="./logs",
-    logging_steps=50,  # Adjust logging frequency
+    logging_steps=50,
     save_steps=100,
     output_dir="./results",
     overwrite_output_dir=True,
-    save_total_limit=3  # Save the latest 3 models for checkpoints
+    save_total_limit=3
 )
 
 trainer = Trainer(
@@ -41,3 +76,7 @@ trainer.train()
 # Save the model and tokenizer
 model.save_pretrained("./custom_gpt2")
 tokenizer.save_pretrained("./custom_gpt2")
+
+# Clean up the temporary file
+import os
+os.remove("temp_training_data.txt")
